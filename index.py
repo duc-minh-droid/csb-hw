69992579e4e6109c7f84c4783566bd3633f687ff
duc-minh-droid <ducminhcsp@gmail.com>
2022-07-16 11:00:59 +0700

    Update index.py

--- index.py
+++ index.py
@@ -26,5 +26,13 @@ def randomList(len):
 num = randomList(10)
 
 def sort(arr):
-    
+    for i in range(len(arr)):
+            j = i
+            while arr[j-1] > arr[j] and j > 0:
+                arr[j-1], arr[j] = arr[j], arr[j-1]
+                j -= 1
+    return arr
+
+print(sort(num))
+        
     
